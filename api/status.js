1d29e8f8b3c0de2b8cec4f21739daf6f31101a1d
Ehorik444 <155249424+Ehorik444@users.noreply.github.com>
2026-03-28 15:06:03 +0300

    Update status.js

--- api/status.js
+++ api/status.js
@@ -1,1 +1,33 @@
+const { Query } = require('mcquery');
 
+async function getServerStatus(host, port = 25565) {
+  return new Promise((resolve) => {
+    const query = new Query(host, port);
+    query.connect((err) => {
+      if (err) {
+        return resolve({ online: false, error: err.message });
+      }
+      query.full_stat((err, stats) => {
+        if (err) {
+          query.close();
+          return resolve({ online: false, error: err.message });
+        }
+        query.close();
+        resolve({
+          online: true,
+          players_online: stats.numplayers,
+          max_players: stats.maxplayers,
+          players: stats.playerlist || [],
+          motd: stats.motd,
+          version: stats.version
+        });
+      });
+    });
+    setTimeout(() => {
+      query.close();
+      resolve({ online: false, error: 'Timeout' });
+    }, 5000);
+  });
+}
+
+module.exports = getServerStatus;
