074c0343dd75a9d7c60fd63bf927a456620c92f0
Ehorik444 <155249424+Ehorik444@users.noreply.github.com>
2026-03-28 15:35:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,19 @@
+// Пример кода для бота (bot.js)
+const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot('YOUR_BOT_TOKEN', { polling: true });
 
+const WEB_APP_URL = 'https://ваше-имя.bothost.ru';
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  
+  bot.sendMessage(chatId, '🦊 Добро пожаловать на сервер Fox SMP!', {
+    reply_markup: {
+      inline_keyboard: [
+        [{ text: '🎮 Открыть сервер', web_app: { url: WEB_APP_URL } }],
+        [{ text: '💬 Чат сервера', url: 'https://t.me/ваш_чат' }],
+        [{ text: '📖 Правила', callback_data: 'rules' }]
+      ]
+    }
+  });
+});
