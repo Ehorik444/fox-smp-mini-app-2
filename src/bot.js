0aa3304ed60002d457c7e1b2addf4d2510818e76
Ehorik444 <155249424+Ehorik444@users.noreply.github.com>
2026-04-12 14:17:41 +0300

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,8 +1,1 @@
-const TelegramBot = require('node-telegram-bot-api');
-require('dotenv').config();
-
-const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {
-  polling: true
-});
-
-module.exports = bot;
+require('../index');
