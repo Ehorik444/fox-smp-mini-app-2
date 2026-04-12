a0f8f5de23ec1aefcb822736f1e57aae8e72e9f1
Ehorik444 <155249424+Ehorik444@users.noreply.github.com>
2026-04-12 18:34:46 +0300

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -19,7 +19,7 @@ const pool = new Pool({
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
-  ssl: { rejectUnauthorized: false }
+  ssl: false
 });
 
 // ===== ANTI-SPAM =====
